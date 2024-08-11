c7ba2feedbbe45cf24aebd118881dd45b94d791d
sandra <sandraakovacevicc@gmail.com>
2024-08-11 16:35:05 +0200

    home screen changes

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -32,7 +32,6 @@ class _MainScreenState extends State<MainScreen> {
     });
 
     if (jwtToken != null) {
-      print('Loaded JWT token: $jwtToken');
       final user = await authService.getUserFromJwt(jwtToken!);
       if (user != null) {
         final userData = await authService.getUserData(user.jmbg, jwtToken!, user.role);
@@ -111,15 +110,115 @@ class _MainScreenState extends State<MainScreen> {
             Padding(
               padding: const EdgeInsets.all(18.0),
               child: user != null
-                  ? Text('Welcome, ${user.name} ${user.surname}', style: const TextStyle(fontSize: 20))
-                  : const Text('Welcome', style: TextStyle(fontSize: 24)),
+                  ? Text('Welcome, ${user.name} ${user.surname}', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+                  : const Text('Welcome', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
             ),
             Expanded(
-              child: Container(
-                width: double.infinity,
-                height: double.infinity,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20.0),
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 8.0,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'About our Fitness Hub!',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'At Fitness Hub, we are dedicated to helping you achieve your fitness goals. Whether you are looking to build muscle, lose weight, or improve your overall health, our state-of-the-art facilities and expert trainers are here to support you every step of the way.',
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20.0),
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFE6FE58),
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 8.0,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Meet Our Trainers',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Our team of professional trainers is here to help you reach your fitness potential. With years of experience and a passion for fitness, they offer personalized training programs tailored to your needs. Get to know them and find the perfect trainer for you!',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                        child: Text(
+                          'Our Trainers',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: SizedBox(
+                        height: 150,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            _trainerAvatar('https://nationalpti.org/wp-content/uploads/2014/02/Personal-Trainer.jpg', 'Zika Zikic'),
+                            _trainerAvatar('https://media.istockphoto.com/id/675179390/photo/muscular-trainer-writing-on-clipboard.jpg?s=612x612&w=0&k=20&c=9NKx1AwVMpPY0YBlk5H-hxx2vJSCu1Wc78BKRM9wFq0=', 'Pera Peric'),
+                            _trainerAvatar('https://www.shutterstock.com/image-photo/portrait-female-personal-trainer-holding-260nw-2249557387.jpg', 'Sandra Kovacevic'),
+                            _trainerAvatar('https://www.shutterstock.com/image-photo/portrait-female-personal-trainer-holding-260nw-2249557387.jpg', 'Sandra Kovacevic'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -129,6 +228,22 @@ class _MainScreenState extends State<MainScreen> {
       bottomNavigationBar: BottomNavBar(
         selectedIndex: _selectedIndex,
         onItemTapped: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _trainerAvatar(String imageUrl, String name) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(imageUrl),
+          ),
+          const SizedBox(height: 8),
+          Text(name, style: const TextStyle(fontSize: 16)),
+        ],
       ),
     );
   }
